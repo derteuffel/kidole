f5a87a7a5024cf60997a7059aa8ab4597ce87bf4
EDOUGA jean patrick <edougajean@gmail.com>
2020-02-14 12:22:51 +0100

    initialisation des données

--- src/main/java/com/derteuffel/kidole/KidoleApplication.java
+++ src/main/java/com/derteuffel/kidole/KidoleApplication.java
@@ -1,13 +1,37 @@
 package com.derteuffel.kidole;
 
+import com.derteuffel.kidole.service.IKidoleInitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KidoleApplication {
+public class KidoleApplication implements CommandLineRunner {
+
+    @Autowired
+    private IKidoleInitService iKidoleInitService;
 
     public static void main(String[] args) {
         SpringApplication.run(KidoleApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        iKidoleInitService.initAccreditations();
+        iKidoleInitService.initCompetDisciplines();
+        iKidoleInitService.initComptes();
+        iKidoleInitService.initConfrontations();
+        iKidoleInitService.initConfrontationTeams();
+        iKidoleInitService.initDisciplines();
+        iKidoleInitService.initPoules();
+        iKidoleInitService.initSites();
+        iKidoleInitService.initTeams();
+        iKidoleInitService.initUsers();
+        iKidoleInitService.initUserTeams();
+        iKidoleInitService.initPouleSite();
+        iKidoleInitService.initUserCompet();
+        iKidoleInitService.initCompetitions();
+
+    }
 }
