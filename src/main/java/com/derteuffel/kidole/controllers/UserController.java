c72f58b7b6ad913a58f0d781858aca4a0bf573ef
derteuffel <derteuffel0@gmail.com>
2020-02-14 09:11:32 +0100

    adding user controller class with implemented methods

---
+++ src/main/java/com/derteuffel/kidole/controllers/UserController.java
@@ -0,0 +1,135 @@
+package com.derteuffel.kidole.controllers;
+
+import com.derteuffel.kidole.entities.User;
+import com.derteuffel.kidole.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/kidole/users")
+public class UserController {
+
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping("/country/{country}")
+    public ResponseEntity<List<User>> findAllByCountry(@PathVariable String country) {
+        List<User> users = new ArrayList<>();
+        try {
+            userRepository.findAllByCountry(country).forEach(users :: add);
+            if (users.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(users,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>((List<User>) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/region/{region}")
+    public ResponseEntity<List<User>> findAllByRegion(@PathVariable String region) {
+        List<User> users = new ArrayList<>();
+        try {
+            userRepository.findAllByRegion(region).forEach(users :: add);
+            if (users.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(users,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>((List<User>) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/ville/{ville}")
+    public ResponseEntity<List<User>> findAllByVille(@PathVariable String ville) {
+
+        List<User> users = new ArrayList<>();
+        try {
+            userRepository.findAllByVille(ville).forEach(users :: add);
+            if (users.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(users,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>((List<User>) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = new ArrayList<>();
+        try {
+            userRepository.findAll().forEach(users :: add);
+            if (users.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(users,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>((List<User>) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User>  save(@RequestBody User user) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        user.setBirthday(dateFormat.format(user.getBirthday()));
+        try {
+            User _user = userRepository.save(user);
+            return new ResponseEntity<>(_user, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>((User) null, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Long id) {
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@RequestBody User user, @PathVariable Long id){
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if (userOptional.isPresent()){
+            User _user = userOptional.get();
+            _user.setCountry(user.getCountry());
+            _user.setBirthday(user.getBirthday());
+            _user.setEmail(user.getEmail());
+            _user.setFirstname(user.getFirstname());
+            _user.setLastname(user.getLastname());
+            _user.setPhone(user.getPhone());
+            _user.setVille(user.getVille());
+            _user.setRegion(user.getRegion());
+            userRepository.save(_user);
+
+            return new ResponseEntity<>(userRepository.save(_user),HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
