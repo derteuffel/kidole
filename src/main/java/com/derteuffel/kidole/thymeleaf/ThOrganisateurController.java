ddc3fab8cac514d94decc5efe92c764521b8196d
django123 <edougajean@gmail.com>
2020-04-15 13:59:33 +0100

    crud accreditation

--- src/main/java/com/derteuffel/kidole/thymeleaf/ThOrganisateurController.java
+++ src/main/java/com/derteuffel/kidole/thymeleaf/ThOrganisateurController.java
@@ -48,6 +48,35 @@ public class ThOrganisateurController {
     @Autowired
     private CompteRepository compteRepository;
 
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private ArbitreRepository arbitreRepository;
+
+    @Autowired
+    private ComiteRepository comiteRepository;
+
+    @Autowired
+    private AthleteRepository athleteRepository;
+
+    @Autowired
+    private DignitaireRepository dignitaireRepository;
+
+    @Autowired
+    private OfficielRepository officielRepository;
+
+    @Autowired
+    private SparingRepository sparingRepository;
+
+    @Autowired
+    private EntraineurRepository entraineurRepository;
+
+    @Autowired
+    private UserRepository  userRepository;
+
+
+
     @Value("${file.upload-dir}")
     private  String fileStorage;
 
@@ -196,8 +225,6 @@ public class ThOrganisateurController {
         return "coordinator/discipline/detail";
     }
 
-    @Autowired
-    private TeamRepository teamRepository;
 
 
     @PostMapping("/teams/save/{id}")
@@ -211,4 +238,157 @@ public class ThOrganisateurController {
 
         return "redirect:/coordinator/kidole/discipline/detail/"+discipline.getId();
     }
+
+
+    /* Accreditation */
+
+
+    @GetMapping("/accreditation/all")
+    public String accreditation(HttpServletRequest request, Model model){
+        Principal principal = request.getUserPrincipal();
+        Compte compte = compteService.findByUsername(principal.getName());
+        request.getSession().setAttribute("accreditations",compte);
+        request.getSession().setAttribute("home","MES ACCREDITATIONS");
+        //Collection<Accreditation> accreditations;
+        model.addAttribute("accreditations",accreditationRepository.findAll());
+        model.addAttribute("accreditation",new Accreditation());
+        return "coordinator/accreditation/lists";
+    }
+
+
+    @GetMapping("/accreditation/detail/{id}")
+    public String findAccreditationById(@PathVariable Long id, Model model){
+        Accreditation accreditation = accreditationRepository.getOne(id);
+        model.addAttribute("accreditation",accreditation);
+        return "coordinator/accreditation/detail";
+    }
+
+
+    @GetMapping("/accreditation/delete/{id}")
+    public String deleteAccreditation(@PathVariable Long id, Model model){
+
+        Accreditation accreditation = accreditationRepository.findById(id)
+                .orElseThrow(() ->new IllegalArgumentException("invalid accreditation id:" +id));
+        accreditationRepository.deleteById(id);
+        model.addAttribute("accreditation", accreditationRepository.findAll());
+
+        return "redirect://accreditation/all";
+
+    }
+
+
+    @PostMapping("/accreditation/{teamId}/{competitionId}")
+    public String saveAccreditation (Model model, Accreditation accreditation, @PathVariable Long teamId, @PathVariable Long competitionId){
+
+        Optional<User> user = userRepository.findByFirstnameOrLastname(accreditation.getName(),accreditation.getName());
+        Team team = teamRepository.getOne(teamId);
+        Competition competition = competitionRepository.getOne(competitionId);
+        accreditation.setCompetition(competition);
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd hh:mm");
+        accreditation.setDate(dateFormat.format(date));
+
+        if (user.isPresent()){
+            accreditation.setUser(user.get());
+            team.getUsers().add(user.get());
+            teamRepository.save(team);
+            user.get().getTeamIds().add(team.getId());
+            user.get().getCompetIds().add(competition.getId());
+            userRepository.save(user.get());
+        }else {
+
+            if (accreditation.getType().contentEquals(EAccreditation.ARBITRE.toString())){
+                Arbitre arbitre = new Arbitre();
+                arbitre.setFirstname(accreditation.getName());
+                arbitre.setLastname(accreditation.getName());
+                arbitreRepository.save(arbitre);
+                accreditation.setUser(arbitre);
+                team.getUsers().add(arbitre);
+                teamRepository.save(team);
+                arbitre.getTeamIds().add(team.getId());
+                arbitre.getCompetIds().add(competition.getId());
+                arbitreRepository.save(arbitre);
+            }else if (accreditation.getType().contentEquals(EAccreditation.ATHLETE.toString())){
+                Athlete athlete = new Athlete();
+                athlete.setFirstname(accreditation.getName());
+                athlete.setLastname(accreditation.getName());
+                athleteRepository.save(athlete);
+                accreditation.setUser(athlete);
+                team.getUsers().add(athlete);
+                teamRepository.save(team);
+                athlete.getTeamIds().add(team.getId());
+                athlete.getCompetIds().add(competition.getId());
+                athleteRepository.save(athlete);
+            }else if (accreditation.getType().contentEquals(EAccreditation.COMITE.toString())){
+                Comite comite = new Comite();
+                comite.setFirstname(accreditation.getName());
+                comite.setLastname(accreditation.getName());
+                comiteRepository.save(comite);
+                accreditation.setUser(comite);
+                team.getUsers().add(comite);
+                teamRepository.save(team);
+                comite.getTeamIds().add(team.getId());
+                comite.getCompetIds().add(competition.getId());
+                comiteRepository.save(comite);
+            }else if (accreditation.getType().contentEquals(EAccreditation.ENTRAINEUR.toString())){
+                Entraineur entraineur = new Entraineur();
+                entraineur.setFirstname(accreditation.getName());
+                entraineur.setLastname(accreditation.getName());
+                entraineurRepository.save(entraineur);
+                accreditation.setUser(entraineur);
+                team.getUsers().add(entraineur);
+                teamRepository.save(team);
+                entraineur.getTeamIds().add(team.getId());
+                entraineur.getCompetIds().add(competition.getId());
+                entraineurRepository.save(entraineur);
+            }else if (accreditation.getType().contentEquals(EAccreditation.DIGNITAIRE.toString())){
+                Dignitaire dignitaire = new Dignitaire();
+                dignitaire.setFirstname(accreditation.getName());
+                dignitaire.setLastname(accreditation.getName());
+                dignitaireRepository.save(dignitaire);
+                accreditation.setUser(dignitaire);
+                team.getUsers().add(dignitaire);
+                teamRepository.save(team);
+                dignitaire.getTeamIds().add(team.getId());
+                dignitaire.getCompetIds().add(competition.getId());
+                dignitaireRepository.save(dignitaire);
+            }else if (accreditation.getType().contentEquals(EAccreditation.OFFICIEL.toString())){
+                Officiciel officiciel = new Officiciel();
+                officiciel.setFirstname(accreditation.getName());
+                officiciel.setLastname(accreditation.getName());
+                officielRepository.save(officiciel);
+                accreditation.setUser(officiciel);
+                team.getUsers().add(officiciel);
+                teamRepository.save(team);
+                officiciel.getTeamIds().add(team.getId());
+                officiciel.getCompetIds().add(competition.getId());
+                officielRepository.save(officiciel);
+            }else {
+                Sparing sparing = new Sparing();
+                sparing.setFirstname(accreditation.getName());
+                sparing.setLastname(accreditation.getName());
+                sparingRepository.save(sparing);
+                accreditation.setUser(sparing);
+                team.getUsers().add(sparing);
+                teamRepository.save(team);
+                sparing.getTeamIds().add(team.getId());
+                sparing.getCompetIds().add(competition.getId());
+                sparingRepository.save(sparing);
+            }
+
+        }
+       accreditationRepository.save(accreditation);
+
+        return "redirect:/accreditation/all";
+
+    }
+
+    @GetMapping("/accreditation/edit/{id}")
+    public String updateAccreditation(Model model, @PathVariable Long id){
+        Accreditation accreditation = accreditationRepository.getOne(id);
+        model.addAttribute("accreditation",accreditation);
+        return "coordinator/accreditation/edit";
+
+    }
+
 }
